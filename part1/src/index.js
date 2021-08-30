926b86fd32e96c8eb6a05f4dca92694679f3fb8d
Cristian González <mr.bombadil@gmail.com>
2021-08-30 23:00:15 +0200

    Segunda versión

--- part1/src/index.js
+++ part1/src/index.js
@@ -20,13 +20,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const setToValue = (rank) => {
+    switch (rank){
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      default: break;      
+    }
+    setAll(all + 1);
+  }
 
   return (
     <div>
       <Title text="Give FeedBack" />
-      <Button handleClick={() => setGood(good +1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />      
+      <Button handleClick={() => setToValue("good")} text="good" />
+      <Button handleClick={() => setToValue("neutral")} text="neutral" />
+      <Button handleClick={() => setToValue("bad")} text="bad" />      
       
       <Title text="Statistics" />
       <Ranking text="Good" value={good} />
