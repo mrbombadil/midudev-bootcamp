3b14b3dee6191b2a4ece93b36a000c491b9319a6
Cristian González <mr.bombadil@gmail.com>
2021-08-31 00:31:14 +0200

    Cuarta versión

--- part1/src/index.js
+++ part1/src/index.js
@@ -11,31 +11,39 @@ const Title = ({text}) => (
   <h1>{text}</h1>
 )
 
-const Ranking = ({text, value}) => (
+const Ranking = ({text, value}) => (  
   <p>{text}: {value}</p>
 )
 
 const App = () => {
   // save clicks of each button to its own state
-  /*const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)*/
-
   const [ranking, setRanking] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
-    all: 0
+    all: 0,
+    avg: 0,
+    positives: 0
   })
 
   const setToValue = rank => {
-    ranking[rank] !== undefined && setRanking({
-      ...ranking,
-      [rank]: ranking[rank] + 1,
-      all: ranking.all + 1 
-    });    
-  }
+    if(ranking[rank] !== undefined){
+      let newRanking = {
+        ...ranking,
+        [rank]: ranking[rank] + 1,      
+        all: ranking.all + 1      
+      }
+
+      newRanking = {
+        ...newRanking,      
+        avg: ((ranking.good - ranking.bad) * 100)/ ranking.all,
+        positives: (ranking.good * 100) / ranking.all
+      }
+
+      setRanking(newRanking);
+    }
+
+  }  
 
   return (
     <div>
@@ -48,10 +56,14 @@ const App = () => {
       <Ranking text="Good" value={ranking.good} />
       <Ranking text="Neutral" value={ranking.neutral} />
       <Ranking text="Bad" value={ranking.bad} />
-      <Ranking text="All" value={ranking.good + ranking.bad + ranking.neutral} />
+      <Ranking text="All" value={ranking.all} />
+      <Ranking text="Average" value={ranking.avg} />
+      <Ranking text="Positives" value={ranking.positives + '%'} />
     </div>
   )
 }
+
+
 
 ReactDOM.render(<App />, 
   document.getElementById('root')
