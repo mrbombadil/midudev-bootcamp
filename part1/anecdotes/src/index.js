54f32a0c96470397d194cb98d36172c9abb28590
Cristian González <mr.bombadil@gmail.com>
2021-09-01 13:23:56 +0200

    Ejercicio completado sin muchos componentes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(props.anecdotes.length).fill(0));
+  
+  const randomNumber = () => {
+    const randomSelection = parseInt((Math.random() * 6), 10);
+    setSelected(randomSelection);    
+  }
+
+  const vote = () => {
+    const copy = [ ...points ];
+    copy[selected] = copy[selected] === undefined ? 1 : ++copy[selected];    
+    setPoints(copy); 
+  }
+
+  const maxVotedAnecdote = points.indexOf(Math.max(...points));
+
+  return (
+    <div>
+      <p>{props.anecdotes[selected]} - votos: {points[selected]}</p>
+      <button onClick={randomNumber}>Siguiente</button><button onClick={vote}>Votar</button>
+      <h2>El más votado:</h2>
+      <p>{props.anecdotes[maxVotedAnecdote]} - votos: {points[maxVotedAnecdote]}</p>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
